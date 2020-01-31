3f4dee6d706fdbb58f8e4e4ef05c6eaa1753e458
Aaron Lerner <alerner@yahoo.com>
2020-01-30 19:35:36 -0800

    ready for pull request

--- client/src/components/Available.test.jsx
+++ client/src/components/Available.test.jsx
@@ -3,7 +3,12 @@ import { shallow, mount } from 'enzyme';
 import Available from './Available';
 import renderer from 'react-test-renderer';
 
+//enzyme-jest
 const wrapper = mount(<Available/>);
+
+//react-test-renderer
+const testRenderer = renderer.create(<Available/>);
+const
 
 describe ('available', () => {
   test('unordered list exists test', () => {
